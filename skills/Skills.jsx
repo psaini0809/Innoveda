9184be6bd90b42c665fc6003bd6903b0ce1c873c
psaini0809 <145288512+psaini0809@users.noreply.github.com>
2025-09-28 22:37:23 +0530

    ccc

--- skills/Skills.jsx
+++ skills/Skills.jsx
@@ -6,7 +6,7 @@ function Skills() {
   return (
     <section
       id="skills"
-      className="py-1 pb-24 px-[12vw] bg-skills-gradient clip-path-custom"
+      className="py-1 pb-24 px-4 sm:px-[12vw] bg-skills-gradient clip-path-custom"
     >
       {/* Section title */}
       <div className="text-center mb-8">
@@ -19,19 +19,19 @@ function Skills() {
       </div>
 
       {/* Skills Categories */}
-      <div className="flex flex-wrap gap-5 py-10 justify-between">
+      <div className="flex flex-wrap gap-5 py-10 justify-center sm:justify-between">
         {SkillsInfo.map((category) => (
           <div
             key={category.title}
-            className="bg-gray-900 backdrop-blur-md px-6 py-6 mb-10 w-full sm:w-[48%] rounded-2xl border border-white shadow-[0_0_20px_1px_rgba(130,69,236,0.3)]"
+            className="bg-gray-900 backdrop-blur-md px-4 sm:px-6 py-6 mb-10 w-full sm:w-[48%] rounded-2xl border border-white shadow-[0_0_20px_1px_rgba(130,69,236,0.3)]"
           >
-            <h3 className="text-2xl font-semibold text-gray-400 mb-4 text-center">
+            <h3 className="text-xl sm:text-2xl font-semibold text-gray-400 mb-4 text-center">
               {category.title}
             </h3>
 
             {/* Skill Items */}
             <Tilt
-              className="w-full max-w-sm mx-auto"
+              className="w-full max-w-full mx-auto"
               tiltMaxAngleX={20}
               tiltMaxAngleY={20}
               perspective={1000}
@@ -39,18 +39,20 @@ function Skills() {
               transitionSpeed={1000}
               gyroscope={true}
             >
-              <div className="grid grid-cols-3 gap-4 w-full">
+              <div className="grid grid-cols-2 sm:grid-cols-3 gap-3 w-full">
                 {category.skills.map((skill) => (
                   <div
                     key={skill.name}
-                    className="flex items-center justify-center space-x-2 bg-transparent border-2 border-gray-700 rounded-3xl py-2 px-2 text-center"
+                    className="flex flex-col sm:flex-row items-center justify-center space-x-0 sm:space-x-2 bg-transparent border-2 border-gray-700 rounded-3xl py-2 px-2 text-center overflow-hidden"
                   >
                     <img
                       src={skill.logo}
                       alt={`${skill.name} logo`}
-                      className="w-8 h-8"
+                      className="w-6 h-6 sm:w-8 sm:h-8"
                     />
-                    <span className="text-sm text-gray-300">{skill.name}</span>
+                    <span className="text-xs sm:text-sm text-gray-300 mt-1 sm:mt-0">
+                      {skill.name}
+                    </span>
                   </div>
                 ))}
               </div>
